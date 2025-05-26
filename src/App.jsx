0de21783558a7d1a2c94fe0e023102bb90094338
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2025-05-26 12:04:12 -0500

    separating useEffect to handle multiple changes on the page separately

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState, useEffect } from 'react';
 
 const App = () => {
     let [count, setCount] = useState(0);
+    let [animal, setAnimal] = useState(
+        ['dog', 'cat', 'bird', 'fish'][Math.floor(Math.random() * 4)]
+    );
 
     const addIncrement = () =>  setCount(count + 1);
     const addDecrement = () => setCount(count - 1);
     const resetZero = () => setCount(0);
 
     useEffect(() => {
-        console.log('useEffect called');
-    });
+        console.log(animal);
+    }, [animal]);
+    
+    useEffect(() => {
+        console.log(count);
+    }, [count]);
 
   return (
     <>
@@ -17,6 +24,12 @@ const App = () => {
       <button onClick={addIncrement}> +1 </button>
       <button onClick={addDecrement}> -1 </button>
       <button onClick={resetZero}> Set to zero </button>
+      <hr/>
+      <h3>{animal}</h3>
+      <button onClick={() => setAnimal(['dog', 'cat', 'bird', 'fish'][Math.floor(Math.random() * 4)])}>
+        Change animal
+        </button>
+
     </>
   );
 }
